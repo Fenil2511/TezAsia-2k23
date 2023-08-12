65ce815601f722d8c1efe5117c4235728269728e
Fenil chauhan <91710798+Fenil2511@users.noreply.github.com>
2023-08-12 23:44:22 +0530

    Add files via upload
    Conduct thorough research on existing DeFi platforms and layer 2 scaling solutions.
    Design the architecture of your DeFi platform, outlining how the layer 2 scaling technology will be integrated to achieve faster transactions and lower gas fees.

---
+++ The ChainTechs/defi-dashboard.js
@@ -0,0 +1,46 @@
+import web3 from "web3";
+import Moralis from "moralis";
+
+const app = Moralis.Web3Provider.create();
+
+const dashboard = {
+  assets: [],
+  transactions: [],
+  yields: [],
+};
+
+app.on("accounts", (accounts) => {
+  dashboard.assets = accounts.map((account) => {
+    return app.eth.getBalance(account);
+  });
+});
+
+app.on("transactions", (transactions) => {
+  dashboard.transactions = transactions;
+});
+
+app.on("yields", (yields) => {
+  dashboard.yields = yields;
+});
+
+function renderDashboard() {
+  const html = `
+    <h1>DeFi Dashboard</h1>
+    <h2>Assets</h2>
+    <ul>
+      ${dashboard.assets.map((asset) => `<li>${asset}</li>`).join("")}
+    </ul>
+    <h2>Transactions</h2>
+    <ul>
+      ${dashboard.transactions.map((transaction) => `<li>${transaction}</li>`).join("")}
+    </ul>
+    <h2>Yields</h2>
+    <ul>
+      ${dashboard.yields.map((yieldValue) => `<li>${yieldValue}</li>`).join("")}
+    </ul>
+  `;
+
+  document.querySelector("body").innerHTML = html;
+}
+
+renderDashboard();
